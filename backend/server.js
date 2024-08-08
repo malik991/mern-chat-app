0aa2340d719e08f8ab54550154d827a623475e9d
malik <mubi.emailid@gmail.com>
2024-08-08 10:21:49 +0500

    deployement and update server.js and write script in pkg.json

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"; // built in from node
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -13,6 +14,11 @@ dotenv.config();
 //const app = express(); // deteled from here and add to socket.js
 const PORT = process.env.PORT || 5000;
 
+// for deployment
+// actual path to the root folder "/"
+const __dirname = path.resolve();
+/// deploymnet code end //////
+
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
 
@@ -20,6 +26,20 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+// for deployment
+//express provide static middleware which used to serve static file i.e html,css,js,imgs,sound etc
+// when we run npm run build it create the bild file in dist on the server
+// in dist folder all static files will be vailable using static middleware
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// now other than above routes, server will server the following page
+// now we can run out front end from our server as well
+app.use("*", () => {
+  path.join(__dirname, "/frontend", "dist", "index.html");
+});
+
+/// deploymnet code end //////
 
 // instead of app.listed we use server from socket
 server.listen(PORT, () => {
